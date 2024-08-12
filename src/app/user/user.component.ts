45a58b58b4bfb7a247fb8cb23a1f7cfd74499244
Daniel Marin <d.i.marinmora@gmail.com>
2024-08-12 17:30:10 -0400

    feat: Flexible components, inputs, requiered inputs

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
-
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -10,17 +7,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]); 
+  @Input( {required:true} ) avatar!:string;
+  @Input( {required:true} ) name!:string;
 
-  /* get imagePath () {
-    return 'assets/users/' + this.selectedUser.avatar;
-  } */
-
-  imagePath = computed( () => 'assets/users/' + this.selectedUser().avatar )
+  
+  public get imagePath() : string {
+    return '/assets/users/' + this.avatar;
+  }
+  
 
   onSelectedUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
-    this.selectedUser.set (DUMMY_USERS[randomIndex]);
   }
 }
 
